c5201403388cf2e4218095894d95500b258304c2
diegobsailva10 <diegobdasilva12@gmail.com>
2022-09-30 13:59:12 -0300

    Operações Aritméticas

---
+++ src/main/kotlin/operadoresAritmeticos/OperadoresAritmeticas.kt
@@ -0,0 +1,28 @@
+fun main (){
+    //Adição
+    var n1 = 30
+    var n2 = 31
+    println("A soma dos números é ${n1 + n2} ")
+
+    //Subtração
+    var numeroSubtracao1 = 50
+    var numeroSubtracao2 = 5
+    println("A substração dos números é: ${numeroSubtracao1 - numeroSubtracao2}")
+
+    //Multiplicação
+    var numeroMultiplicacao1 = 2
+    var numeroMultiplicacao2 = 2
+    println("A multiplicação dos números é: ${numeroMultiplicacao1 * numeroMultiplicacao2}")
+
+    //Divisão
+    var numeroDivisao1 = 2
+    var numeroDivisao2 = 2
+    println("A divisão dos números é: ${numeroDivisao1 / numeroDivisao2 }")
+
+    //Resto da divisão
+
+    var restoDaDivisao = 10 % 2
+    println("O resto da divisão do números é: ${restoDaDivisao}")
+
+
+}
