fb6454720c73d5f33365acd188b1a498144826cb
diegobsailva10 <diegobdasilva12@gmail.com>
2022-09-30 22:21:08 -0300

    commit

--- src/main/kotlin/strings/inferenciaDeTipo.kt
+++ src/main/kotlin/strings/inferenciaDeTipo.kt
@@ -1,10 +1,13 @@
 package strings
 fun main(){
+    //Sem inferencia de tipo
     var nome2: String = "Diego Silvaasaaaa"
     var idade: Int = 10
     var numeroDePontoFlutante: Float = 1.8F
     var numeroDouble: Double = 1.9
 
+    //Com Inferência de Tipo
 
-
+    var nome3 = "Programação"
+    var numeroAleatorio = 1
 }
